ec4d40c27f912dbd21ae8a23f7bef478e7d154aa
deauth <anurage66@gmail.com>
2024-03-14 23:53:36 +0530

    fix: offerings responsiveness

--- src/components/Offerings/Offerings.tsx
+++ src/components/Offerings/Offerings.tsx
@@ -5,7 +5,7 @@ import FloatingCard from "./FloatingCard";
 const Offerings = () => {
 	return (
 		<div className=" w-full flex flex-col lg:pt-10 pt-5 mx-auto h-fit pb-10 bg-dot-blue-100/[0.8] ">
-			<div className="grid md:grid-cols-6 grid-cols-2 gap-5 w-full mx-auto mt-auto lg:px-0 px-3">
+			<div className="grid md:grid-cols-6 grid-cols-2 gap-5 w-fit mx-auto mt-auto lg:px-0 px-3 ">
 				{OfferingCardsStatic.map((item, idx) => {
 					return (
 						<FloatingCard
